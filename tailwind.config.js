4ed41c1ee7465b9182c55ed09e9a1fdecf57bc14
J. Austin Hughey <github@jah.io>
2023-06-21 09:03:24 -0500

    Transition to using Tailwind
    Remove some unused cruft, set up use of Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,vue,jsx,ts,tsx}', './index.html'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// https://v2.tailwindcss.com/docs/guides/vue-3-vite
