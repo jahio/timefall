2acf4f68503b6ef402f89ec0e0a4699963edeafb
J. Austin Hughey <github@jah.io>
2023-06-20 23:14:11 -0500

    Minor cleanup
    Also adds some basic verbiage on front page to introduce the tool (TODO: make this more succinct)

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
+      <div>
+        <h1>TIMEFALL</h1>
+        <p>Timefall is a tool that allows you to instantly render an interactive
+          timeline from any valid YAML resource publicly available on the internet.
+          There's no build step or backend server process required; it all executes
+          within the browser. Simply feed it a URL and click "Go" to start.
+        </p>
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@ import Timeline from '../components/Timeline.vue'
 // This URL below is a starter URL with sample data. Feed it your own! Any valid YAML with the right
 // structural shape will work. See the sample for an idea of what's expected.
 const yamlUrl = ref('https://gist.githubusercontent.com/jahio/5fb101ceef40eb5e97406ba9f325173f/raw/american-revolution-timeline.yaml');
-// const yamlUrl = ref('http://localhost:5173/usa.yaml');
 </script>
 
 <template>
