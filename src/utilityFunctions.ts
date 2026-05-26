0586bf4f4b0d6b7b9216479b0054cdae2f80d3d0
J. Austin Hughey <git@jah.io>
2026-05-26 19:55:25 -0400

    Add utilities for mobile detection

---
+++ src/utilityFunctions.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(maxWidth: Number) {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(`(max-width: ${maxWidth}px)`).matches
+  );
+
+  useEffect(() => {
+    const mq = window.matchMedia(`(max-width: ${maxWidth}px)`);
+    // const handler = e => setMatches(e.matches);
+    const handler = (e: MediaQueryListEvent) => {
+      setMatches(e.matches);
+    };
+    mq.addEventListener('change', handler);
+    return () => mq.removeEventListener('change', handler); // cleanup
+  }, [maxWidth]);
+  return matches;
+}
